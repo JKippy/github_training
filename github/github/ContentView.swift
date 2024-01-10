a2c859304ff9ff1c82d73972b9a1c380b4869ab4
JKippy <kaipjason@gmail.com>
2024-01-10 10:39:14 -0500

    initial commit

---
+++ github/github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github
+//
+//  Created by Jason Kaip on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! hi again")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github/github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Jason Kaip on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
